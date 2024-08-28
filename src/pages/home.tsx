ece11fec8d0ac9ea0e29e2fe5226055340eeb023
omarfaruktaj <omarfaruk3393@gmail.com>
2024-08-28 14:22:18 +0600

    feat(home): add Service Advertisement Section
    - Display Service Advertisement section in home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/features/home/components/hero";
+import ServiceAdvertisement from "@/features/home/components/service-advertisement";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <ServiceAdvertisement />
     </div>
   );
 }
